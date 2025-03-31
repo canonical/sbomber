f1add91db0799884a09c62eac7eed69094aa358a
Pietro Pasotti <pietro.pasotti@canonical.com>
2025-03-31 17:01:53 +0200

    rolled back manifest changes

--- tests/test_sbomb_client.py
+++ tests/test_sbomb_client.py
@@ -7,8 +7,9 @@ from unittest.mock import patch, MagicMock
 import pytest
 import yaml
 
+from clients.client import ProcessingStatus
 from clients.sbom import DEFAULT_SERVICE_URL
-from sbomber import DEFAULT_MANIFEST, DEFAULT_PACKAGE_DIR, prepare, DEFAULT_STATEFILE, submit
+from sbomber import DEFAULT_MANIFEST, DEFAULT_PACKAGE_DIR, prepare, DEFAULT_STATEFILE, submit, SBOMB_KEY, SECSCAN_KEY
 
 
 @pytest.fixture(autouse=True)
@@ -64,7 +65,7 @@ def sbomber_post_mock(project: Path):
         mm.status_code = 200
 
         if url.endswith("/upload"):
-            mm.json.return_value = {"data": {"artifactId": "foo"}}
+            mm.json.return_value = {"data": {"artifactId": "this-is-a-testing-sbomber-token"}}
         return mm
 
     with patch("requests.post", side_effect=get_mm) as mm:
@@ -80,7 +81,7 @@ def secscanner_run_mock(project: Path):
         elif command == 'report':
             return "<some html>"
         elif command == 'submit':
-            return "this-is-a-testing-token"
+            return "this-is-a-testing-secscanner-token Scan request submitted."
         else:
             raise ValueError(command)
 
@@ -153,6 +154,34 @@ def test_prepare_statefile(project, tmp_path, sbomber_get_mock, sbomber_post_moc
 def test_submit(project, tmp_path, sbomber_get_mock, sbomber_post_mock, secscanner_run_mock):
     mock_dev_env(project, prepared=True)
     submit()
-    assert not sbomber_get_mock.called
-    assert sbomber_post_mock.call_count == 3
+    assert sbomber_get_mock.called
+    assert sbomber_post_mock.call_count == 6  # 1 chunk and 1 complete call each
     assert secscanner_run_mock.call_count == 3
+
+    assert yaml.safe_load((project / DEFAULT_STATEFILE).read_text()) == {
+        'artifacts': [
+            {
+                "name": name,
+                "source": str(tmp_path / f"{name}.{type}"),
+                "object": str(tmp_path / f"{name}.{type}"),
+                SBOMB_KEY: {
+                    "this-is-a-testing-sbomber-token": ProcessingStatus.pending.value
+                },
+                SECSCAN_KEY: {
+                    "this-is-a-testing-secscanner-token": ProcessingStatus.pending.value
+                },
+                "type": type,
+            }
+            for name, type in (
+                ('foo', 'charm'),
+                ('bar', 'rock'),
+                ('baz', 'snap'),
+            )
+        ],
+        'department': 'charming_engineering',
+        'email': 'luca.bello@canonical.com',
+        'generate': ['sbom', 'secscan'],
+        'sbom-service-url': DEFAULT_SERVICE_URL,
+        'team': 'observability'
+    }
+
