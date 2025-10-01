1d3d8f291e2d39d172feae5893ad276dc092ffbc
Paulo Machado <paulo.machado@canonical.com>
2025-10-01 13:45:13 -0300

    address downloaded artifacts filename clash

--- src/sbomber.py
+++ src/sbomber.py
@@ -697,9 +697,7 @@ def download(statefile: Path = DEFAULT_STATEFILE, reports_dir=DEFAULT_REPORTS_DI
                     "Consider `polling` first."
                 )
 
-            filename = (
-                f"{artifact_name}.{client_name}{'.html' if client_name == 'secscan' else '.json'}"
-            )
+            filename = f"{artifact.type.value}-{artifact_name}.{client_name}{'.html' if client_name == 'secscan' else '.json'}"
 
             done.append((f"({client_name}):{artifact.name}", filename))
 
